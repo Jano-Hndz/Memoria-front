9adfd98f69939858daf5a7f8fd3c670aeb8c5df9
Alejandro Hernández <alejandro.hernandezh@sansano.usm.cl>
2024-06-26 19:48:25 -0400

    agrega Google Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-
+import ReactGA from "react-ga4";
 import { AppMain } from './AppMain';
 import { store } from './store';
+
+
+ReactGA.initialize("G-GQHETJGSYQ");
+
+ReactGA.send({ hitType: "pageview", page: window.location.pathname});
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
